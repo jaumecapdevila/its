8105434f257d43af88b0ad3bd39848e1759739da
Jaume Capdevila <contact@jaumecapdevila.net>
2019-03-29 10:25:49 +0100

    Remove log

--- src/its.js
+++ src/its.js
@@ -3,7 +3,6 @@ import findTargets from './utils/inputs';
 
 const targets = findTargets();
 
-console.log(targets);
 if (targets.length !== 0) {
   targets.forEach(target => {
     target.addEventListener('keyup', keyUpListener);
